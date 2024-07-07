4d2f01f8846ab5e7d214775f7f0d2cfb2a3a2206
Kei-K23 <keiksl2301@gmail.com>
2024-07-07 23:23:24 +0630

    Add get by id controller method

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using TodoAPI.Contracts;
 using TodoAPI.Interfaces;
-using TodoAPI.Services;
 
 namespace TodoAPI.Controllers
 {
@@ -44,6 +43,58 @@ namespace TodoAPI.Controllers
         catch (Exception ex)
         {
           return StatusCode(500, new {message = "An error occurred while retrieving todos", error = ex.Message});
+        }
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetTodoByIdAsync(Guid id) {
+        try
+        {
+            var todo = await _todoService.GetByIdAsync(id);
+            if (todo == null) {
+    return Ok(new {message = "Todo not found with id: " + id});
+            }
+            return Ok(new {message = "Todo not found with id: " , todo = todo});
+        }
+        catch (Exception ex)
+        {
+              return StatusCode(500, new {message = "An error occurred while getting todo by id", error = ex.Message});
+        }
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task UpdateTodoAsync(Guid id, UpdateTodoRequest updateTodoRequest) {
+        try
+        {
+        var todo = await _todoService.GetByIdAsync(id) ?? throw new Exception("Could not find todo to update");
+
+        if (updateTodoRequest.Title != null) {
+            todo.Title = updateTodoRequest.Title;
+        }
+
+        if (updateTodoRequest.Description != null) {
+            todo.Description = updateTodoRequest.Description;
+        }
+       
+        if (updateTodoRequest.IsComplete != null) {
+            todo.IsComplete = updateTodoRequest.IsComplete.Value;
+        }
+
+        if (updateTodoRequest.DueDate != null)
+         {
+             todo.DueDate = updateTodoRequest.DueDate.Value;
+         }
+
+        if (updateTodoRequest.Priority != null) {
+            todo.Priority = updateTodoRequest.Priority;
+        }
+        todo.UpdatedAt = DateTime.Now;
+        await _todoService.UpdateTodoAsync(id, updateTodoRequest);
+        }
+            catch (System.Exception)
+        {
+            
+            throw;
         }
     }
     }
